6b4432cd685bceeef2ad83e25d0237ce75fa8956
MJ <michaeljouve@gmail.com>
2019-08-01 12:45:57 +0200

    first

---
+++ src/components/searchBox/searchBox.component.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./searchBox.styles.css";
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+  <input className='searchBox' type="search" placeholder={placeholder} onChange={handleChange} />
+);
